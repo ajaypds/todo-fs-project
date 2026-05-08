4a0ac12b75c88606ed68ac8b655740a4e93f1586
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-08 20:52:42 +0530

    created CustomUserDetails in security package

---
+++ backend/src/main/java/com/example/todo/security/CustomUserDetails.java
@@ -0,0 +1,35 @@
+package com.example.todo.security;
+
+import com.example.todo.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public UUID getUserId(){
+        return user.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
