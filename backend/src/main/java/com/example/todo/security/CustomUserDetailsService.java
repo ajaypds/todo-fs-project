7dd97d223afd613a52b8cf23f1c618bb5be36850
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-08 20:58:57 +0530

    created Custom user details service

---
+++ backend/src/main/java/com/example/todo/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.todo.security;
+
+import com.example.todo.user.entity.User;
+import com.example.todo.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
+}
