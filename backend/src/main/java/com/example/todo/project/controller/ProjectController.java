f5921e7f5577a4f919bb635a07339ff553f4d3df
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-12 17:21:12 +0530

    added project controller

---
+++ backend/src/main/java/com/example/todo/project/controller/ProjectController.java
@@ -0,0 +1,50 @@
+package com.example.todo.project.controller;
+
+import com.example.todo.project.dto.CreateProjectRequest;
+import com.example.todo.project.dto.ProjectResponse;
+
+import com.example.todo.project.service.ProjectService;
+
+import com.example.todo.security.CustomUserDetails;
+
+import jakarta.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @PostMapping
+    public ProjectResponse createProject(
+
+            @AuthenticationPrincipal
+            CustomUserDetails userDetails,
+
+            @Valid
+            @RequestBody
+            CreateProjectRequest request
+    ) {
+
+        return projectService.createProject(userDetails.getUserId(),request);
+    }
+
+    @GetMapping
+    public List<ProjectResponse> getProjects(
+
+            @AuthenticationPrincipal
+            CustomUserDetails userDetails
+    ) {
+
+        return projectService.getProjects(userDetails.getUserId());
+    }
+}
