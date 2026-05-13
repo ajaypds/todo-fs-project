30afac28ff2defe6ffbc61cbc4bb59d7a56839fc
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-13 13:27:24 +0530

    added ordering of tasks

--- backend/src/main/java/com/example/todo/task/repository/TaskRepository.java
+++ backend/src/main/java/com/example/todo/task/repository/TaskRepository.java
@@ -10,5 +10,5 @@ import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, UUID> {
 
-    Page<Task> findByUser(User user, Pageable pageable);
+    Page<Task> findByUserOrderByPositionAsc(User user, Pageable pageable);
 }
