0326770dea336798c3c7d9168052194ad28a31e4
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-14 11:39:18 +0530

    added refreshToken property and setter method for refresh token

--- frontend/src/store/authStore.ts
+++ frontend/src/store/authStore.ts
@@ -2,24 +2,31 @@ import { create } from "zustand";
 
 type AuthState = {
     token: string | null;
+    refreshToken: string | null;
 
     setToken: (token: string) => void;
+    setAuth: (token: string, refreshToken: string) => void;
 
     logout: () => void;
 };
 
 export const useAuthStore = create<AuthState>((set) => ({
     token: localStorage.getItem("token"),
+    refreshToken: localStorage.getItem("refreshToken"),
 
     setToken: (token) => {
         localStorage.setItem("token", token);
-
         set({ token });
     },
 
     logout: () => {
         localStorage.removeItem("token");
+        set({ token: null, refreshToken: null });
+    },
 
-        set({ token: null });
+    setAuth: (token, refreshToken) => {
+        localStorage.setItem("token", token);
+        localStorage.setItem("refreshToken", refreshToken);
+        set({ token, refreshToken });
     },
 }));
