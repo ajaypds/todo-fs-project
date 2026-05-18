cf752b90d53457176b6801587df4d90759eb4d3d
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-18 12:40:26 +0530

    applied theme to document

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { DashboardPage } from "./pages/DashboardPage";
-
 import { ProtectedRoute } from "./routes/ProtectedRoute";
+import { useEffect } from "react";
+import { useThemeStore } from "./store/themeStore";
 
 function App() {
+  const theme = useThemeStore((state) => state.theme);
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", theme === "dark");
+  }, [theme]);
+
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
