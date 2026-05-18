16d27efe887a34f6839cb236ebbe579179ae4ac4
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-18 13:15:51 +0530

    removed form x-axis padding to match with other components in the page

--- frontend/src/features/projects/components/CreateProjectForm.tsx
+++ frontend/src/features/projects/components/CreateProjectForm.tsx
@@ -36,7 +36,10 @@ export const CreateProjectForm = ({ onCreate }: Props) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="bg-background rounded-lg p-4 mb-6">
+    <form
+      onSubmit={handleSubmit}
+      className="bg-background rounded-lg py-4 mb-6"
+    >
       <h2 className="font-semibold mb-4">Create Project</h2>
 
       <Input
