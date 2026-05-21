34d443c51db6958e211c863191a8b96724bd855f
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-21 12:39:32 +0530

    websocket client

---
+++ frontend/src/realtime/socket.ts
@@ -0,0 +1,15 @@
+import SockJS from "sockjs-client";
+
+import { Client } from "@stomp/stompjs";
+
+const socket = new SockJS(
+    `${import.meta.env
+        .VITE_API_BASE_URL
+        .replace("/api/v1", "")}/ws`
+);
+
+export const stompClient = new Client({
+
+    webSocketFactory: () => socket,
+    reconnectDelay: 5000,
+});
