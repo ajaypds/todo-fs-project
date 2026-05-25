5569c2520984e6dc2bba57d70e8a70ea4b25b649
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-25 14:55:55 +0530

    replaced 'someone' with user email

--- backend/src/main/java/com/example/todo/realtime/PresenceListener.java
+++ backend/src/main/java/com/example/todo/realtime/PresenceListener.java
@@ -1,11 +1,13 @@
 package com.example.todo.realtime;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.*;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class PresenceListener {
@@ -16,10 +18,12 @@ public class PresenceListener {
     @EventListener
     public void handleConnect(SessionConnectEvent event) {
 
+        log.info("User connected: " + event.getUser().getName());
+
         PresenceEvent payload = PresenceEvent
                         .builder()
                         .type("USER_CONNECTED")
-                        .username("Someone")
+                        .username(event.getUser().getName())
                         .build();
 
         messagingTemplate.convertAndSend("/topic/presence", payload);
