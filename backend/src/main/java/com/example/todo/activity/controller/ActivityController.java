74b958bf926731ee6401b0dc8d85d740833968f7
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-26 21:35:52 +0530

    filtered by current user

--- backend/src/main/java/com/example/todo/activity/controller/ActivityController.java
+++ backend/src/main/java/com/example/todo/activity/controller/ActivityController.java
@@ -2,7 +2,9 @@ package com.example.todo.activity.controller;
 
 import com.example.todo.activity.dto.ActivityResponse;
 import com.example.todo.activity.service.ActivityService;
+import com.example.todo.auth.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -14,7 +16,7 @@ public class ActivityController {
     private final ActivityService service;
 
     @GetMapping
-    public List<ActivityResponse> getActivities() {
-        return service.getActivities();
+    public List<ActivityResponse> getActivities(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return service.getActivities(userDetails.getUserId());
     }
 }
