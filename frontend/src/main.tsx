facef6860b25dd4aee1ebc77f40764281084d9ce
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-26 20:05:40 +0530

    enclosed App with RealtimeProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import "./index.css";
 import App from "./App.tsx";
+import { RealtimeProvider } from "./features/realtime/providers/RealtimeProvider.tsx";
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <RealtimeProvider>
+          <App />
+        </RealtimeProvider>
         <Toaster position="top-right" />
       </BrowserRouter>
     </QueryClientProvider>
