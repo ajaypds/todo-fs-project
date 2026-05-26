6b4e6aa27662e4ee7256b7a066fe7416f4c09d00
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-26 21:31:49 +0530

    added userid property

--- backend/src/main/java/com/example/todo/task/event/TaskUpdatedEvent.java
+++ backend/src/main/java/com/example/todo/task/event/TaskUpdatedEvent.java
@@ -1,18 +1,15 @@
 package com.example.todo.task.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import java.util.UUID;
 
 @Getter
+@AllArgsConstructor
 public class TaskUpdatedEvent {
 
     private final UUID taskId;
     private final String title;
+    private final UUID userId;
 
-    public TaskUpdatedEvent(UUID taskId, String title
-    ) {
-
-        this.taskId = taskId;
-        this.title = title;
-    }
 }
