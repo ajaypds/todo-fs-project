f723a2c123c7de1f5a3faa40fc9faa355787430c
Ajay Prasad <ajay.prasad@outlook.in>
2026-05-26 21:35:03 +0530

    set userId to activity

--- backend/src/main/java/com/example/todo/activity/listener/ActivityEventListener.java
+++ backend/src/main/java/com/example/todo/activity/listener/ActivityEventListener.java
@@ -3,6 +3,7 @@ package com.example.todo.activity.listener;
 import com.example.todo.activity.entity.Activity;
 import com.example.todo.activity.repository.ActivityRepository;
 import com.example.todo.task.event.*;
+import com.example.todo.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 public class ActivityEventListener {
 
     private final ActivityRepository repository;
+    private final UserRepository userRepository;
 
     @EventListener
     public void handleCreated(TaskCreatedEvent event) {
@@ -21,6 +23,7 @@ public class ActivityEventListener {
                         .type("TASK_CREATED")
                         .message("Created task: " + event.getTitle())
                         .createdAt(LocalDateTime.now())
+                        .user(userRepository.findById(event.getUserId()).orElse(null))
                         .build()
         );
     }
